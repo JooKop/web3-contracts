7b2ad63ffaeadfde302f1baf2c211a0594ea0034
Joonas Köppä <joonas.koppa@smartly.io>
2022-09-04 16:42:29 +0300

    Remove arguments from contrct deployment

--- deploy.js
+++ deploy.js
@@ -1,8 +1,8 @@
 // deploy code will go here
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { abi, evm } = require('./utils/getContract')('Inbox.json')
 require('dotenv').config({path: __dirname + '/.env'});
+const { abi, evm } = require('./utils/getContract')(process.env.CONTRACT)
 
 let provider = new HDWalletProvider({
     mnemonic: process.env.MNEMONIC,
@@ -17,7 +17,7 @@ const deploy = async() => {
     const accounts = await web3.eth.getAccounts();
     console.log('Attempting to deploy from account', accounts[0]);
     const result = await new web3.eth.Contract(abi)
-        .deploy({ data: evm.bytecode.object, arguments: ['Hi there!']})
+        .deploy({ data: evm.bytecode.object })
         .send({ gas: '1000000', from: accounts[0] });
     console.log('Contract deployed to', result.options.address)
     provider.engine.stop();
